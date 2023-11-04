9fdbf7e0badf6267912fffff317f3efd4bedcc3d
thecla-glitch <thecla.myalize@gmail.com>
2023-11-04 12:14:31 +0300

    Django blog post project

---
+++ day1.py
@@ -0,0 +1,55 @@
+print('django girls')
+a=1
+b=2
+c=a+b
+print(c)
+
+a=6.0
+
+x=int(a)
+
+print(type(a))
+
+
+print(type(x))
+
+a="pretty aziza"
+
+print(a)
+
+
+mylist=[1,2,'gugu']
+
+print(mylist)
+
+print (mylist[2])
+
+
+
+
+age=input("enter your age:")
+
+y=int(age)
+
+country=input("enter your country")
+
+
+      
+def thecla(age,country):  
+   
+
+
+
+    if y>18 :
+
+        if country=="tanzanian":
+        
+            print("welcome dear child")
+
+        else:
+            print("invalid nationality")
+
+    else:
+        print("your not old enough")
+
+thecla(age=20,country="tanzanian")
